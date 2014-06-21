7b1fe12588994516656b239856ad2e20ebf9d88f
S <s>
2014-06-20 19:26:58 -0700

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -89,6 +89,8 @@ public class Breakout extends GraphicsProgram {
 		//add bricks to graphics window
 		for(int i = 0; i < NBRICKS_PER_ROW; i++){
 			GRect rect = new GRect(xCoord, yCoord, BRICK_WIDTH, BRICK_HEIGHT); 
+			rect.setFilled(true);
+			rect.setFillColor(color);
 			add(rect);
 			xCoord += BRICK_WIDTH + BRICK_SEP;
 		}
