630899391dfafc094047923ff5251213c7a10d2c
S <s>
2014-06-22 12:35:55 -0700

    {"type":"TI", "runs":"4"}

--- Breakout.java
+++ Breakout.java
@@ -60,12 +60,16 @@ public class Breakout extends GraphicsProgram {
 	private static final int DELAY = 20;
 
 /** Runs the Breakout program. */
-	public void run() {
+	
+	//public static void main(String[] args){
+	public static void main(String[] args) {
 	
 		boardSetup();	//sets up the board
 		play();
 
 	}
+	
+	
 	
 	private void play(){
 		addMouseListeners();
