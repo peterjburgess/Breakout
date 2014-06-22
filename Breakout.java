a626658a55d43717994a4c43acce92da675d6808
S <s>
2014-06-22 02:16:59 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -69,7 +69,7 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	public void mouseDragged(MouseEvent e){
-		if (gobj != null){
+		if (gobj == paddle){
 			gobj.move(e.getX() - last.getX(), e.getY() - last.getY());
 			last = new GPoint(e.getPoint());	
 		}
@@ -139,7 +139,7 @@ public class Breakout extends GraphicsProgram {
 	private void addPaddle(){
 		int xCoord = WIDTH/2 - PADDLE_WIDTH / 2;
 		int yPaddle = HEIGHT - PADDLE_Y_OFFSET;
-		GRect paddle = new GRect (xCoord, yPaddle, PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle = new GRect (xCoord, yPaddle, PADDLE_WIDTH, PADDLE_HEIGHT);
 		paddle.setFilled(true);
 		add(paddle);
 		addMouseListeners();
@@ -148,5 +148,5 @@ public class Breakout extends GraphicsProgram {
 	//private instance variables
 	private GObject gobj;
 	private GPoint last; //gives me the last xCoord of the paddle
-
+	private GRect paddle; //gives me the paddle
 }
