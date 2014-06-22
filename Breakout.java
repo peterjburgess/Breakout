d9feb58cd1862a484a9e5af64385df4dec835871
S <s>
2014-06-22 02:54:11 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -69,6 +69,12 @@ public class Breakout extends GraphicsProgram {
 		if(e.getX() >= PADDLE_WIDTH/2 && e.getX() <= getWidth() - PADDLE_WIDTH/2){
 			paddle.setLocation(e.getX() - PADDLE_WIDTH / 2, yPaddle);
 		}
+		else if(e.getX() < PADDLE_WIDTH/2){
+			paddle.setLocation(0, yPaddle);
+		}
+		else {
+			paddle.setLocation(getWidth() - PADDLE_WIDTH, yPaddle);
+		}
 	}
 	
 
